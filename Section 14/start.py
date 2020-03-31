8133e2c0cd892b9bf5e7d55ef52ebbdd2b63111a
luisandre69 <luisandre_69@hotmail.com>
2020-03-31 17:36:20 +0100

    continauation and multiline

--- Section 14/start.py
+++ Section 14/start.py
@@ -5,3 +5,14 @@ print("hello home")
 
 i = input("Please enter your Name --> ")
 print("Your name is --> " + i)
+
+#%%  Multiline
+name=\
+        ("Luis Abreu")
+print(name)
+
+# %% Continuation
+name=("Luis Abreu");print(name)
+
+
+# %%
