b54fdc3915459098d16e056ae5821dbf044c97a0
luisandre69 <luisandre_69@hotmail.com>
2020-04-01 18:47:42 +0100

    list and sum values

--- Section 14/for loops.py
+++ Section 14/for loops.py
@@ -22,3 +22,18 @@ number=input("Please enter a number:")
 for i in range(10,0,-1):
     print(number + ' * ' + str(i) + ' = ' + str(int(number)*i))
 #%% FOR with list
+list1 = [1,3,5,7,9,10,20,"Test","google.com",10.10]
+for i in list1:
+    print(i)
+# %% lists
+for i in 'Testing':
+    print(i)
+# %% for to sum all values
+l={43,56,34,45,77}
+z=0
+for i in l:
+    z=z+i
+print(z)
+
+
+# %%
