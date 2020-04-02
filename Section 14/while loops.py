c7354c06c15662ddb60b65b4cba2226837eb1878
luisandre69 <luisandre_69@hotmail.com>
2020-04-02 19:03:36 +0100

    while loops

--- Section 14/while loops.py
+++ Section 14/while loops.py
@@ -3,10 +3,15 @@ i = 1
 while (i < 10):
     print(i)
     i += 1
-# %%
+# %% while with increment
 number = input("Please enter your number:")
 i = 1
-while (i < 10):
-    print(int(number)*i)
+while (i < 11):
+    print(number + ' * ' + str(i) + ' = ' + str(int(number)*i))
     i += 1
-# %%
+# %% while with decrement
+number = input("Please enter your number:")
+i = 10
+while (i >= 1):
+    print(number + ' * ' + str(i) + ' = ' + str(int(number)*i))
+    i -= 1
