3c7432d8078bc948ae8af884dd6a85bb41534e54
luisandre69 <luisandre_69@hotmail.com>
2020-04-03 13:40:02 +0100

    loops

--- Section 14/loops.py
+++ Section 14/loops.py
@@ -3,12 +3,22 @@ for i in range(1, 11):
     if (i == 5):
         break
     print(i)
-
 # %%
 number = input("Please enter your number:")
 for i in range(1, 11):
-    if(int(number)*i==60):
+    if(int(number)*i == 60):
         break
-    print (int(number)*i)
+    print(int(number)*i)
+# %% continue statement
+number = input("Please enter your number:")
+for i in range(1, 11):
+    if(int(number)*i % 10 == 0):
+        continue
+    print(int(number)*i)
+# %% else statement
+for i in range(1, 11):
+    print(i)
+else:
+    print("Loop is ended")
 
 # %%
