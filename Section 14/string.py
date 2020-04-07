8731c3fa270ed0741b72cba001027dd832dfb35b
luisandre69 <luisandre_69@hotmail.com>
2020-04-07 17:32:40 +0100

    strings

---
+++ Section 14/string.py
@@ -0,0 +1,34 @@
+# %% String
+myUrl = "https://luisabreu.co.uk"
+print(myUrl)
+# %%
+myUrl = "https://'luisabreu'.co.uk"
+print(myUrl)
+myUrl1 = 'https://"luisabreu".co.uk'
+print(myUrl1)
+# %%
+multiline = """
+    This is
+    the use
+    of multilines
+    """
+print(multiline)
+# %%
+multiline = """
+    This is
+    the use
+    of multilines
+    """
+print("Hello " + multiline + " Thank you")
+# %%
+hello = "Hello!!!"
+print(hello*5)
+# %%
+name = input("Please insert your Name:")
+address = input("Please insert your Address:")
+profile = input("Please insert your job profile:")
+
+print("Name: " + name + " Address: " + address + " Job: " + profile)
+
+
+# %%
