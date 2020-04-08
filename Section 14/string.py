19e5328dfdcb425ae169b51186d614de1a55b608
luisandre69 <luisandre_69@hotmail.com>
2020-04-08 18:59:01 +0100

    string

--- Section 14/string.py
+++ Section 14/string.py
@@ -1,34 +1,31 @@
 # %% String
 myUrl = "https://luisabreu.co.uk"
 print(myUrl)
-# %%
+# %% string comments
 myUrl = "https://'luisabreu'.co.uk"
 print(myUrl)
 myUrl1 = 'https://"luisabreu".co.uk'
 print(myUrl1)
-# %%
+# %% multiline
 multiline = """
     This is
     the use
     of multilines
     """
 print(multiline)
-# %%
+# %% concatenation
 multiline = """
     This is
     the use
     of multilines
     """
 print("Hello " + multiline + " Thank you")
-# %%
+# %% multiplication
 hello = "Hello!!!"
 print(hello*5)
-# %%
+# %% input and print values
 name = input("Please insert your Name:")
 address = input("Please insert your Address:")
 profile = input("Please insert your job profile:")
-
 print("Name: " + name + " Address: " + address + " Job: " + profile)
-
-
 # %%
