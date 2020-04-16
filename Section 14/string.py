a5dae6bdb077059f47ce7a1c153af05409ab152d
luisandre69 <luisandre_69@hotmail.com>
2020-04-16 21:02:12 +0100

    fetch strings

--- Section 14/string.py
+++ Section 14/string.py
@@ -28,4 +28,18 @@ name = input("Please insert your Name:")
 address = input("Please insert your Address:")
 profile = input("Please insert your job profile:")
 print("Name: " + name + " Address: " + address + " Job: " + profile)
+# %% string fetch value from any index, just by passing index
+emailAddress = "luisandre_69@hotmail.com"
+print(emailAddress[6])
+# %% fetch substring by starting and end index
+emailAddress = "luisandre_69@hotmail.com"
+print(emailAddress[0:4])
+# %% fetch substring by giving start index only
+emailAddress = "luisandre_69@hotmail.com"
+print(emailAddress[4:])
+# %% fetch substring by giving end index only
+emailAddress = "luisandre_69@hotmail.com"
+print(emailAddress[:4])
+
+
 # %%
