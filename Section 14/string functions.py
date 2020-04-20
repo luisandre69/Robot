720fad35dbdabd8bed4e52d64e76ae358470bb24
luisandre69 <luisandre_69@hotmail.com>
2020-04-20 12:23:33 +0100

    functions replace

--- Section 14/string functions.py
+++ Section 14/string functions.py
@@ -8,7 +8,32 @@ print(emailAddress.upper())
 # %% lower
 emailAddress = "luisandre_69@hotmail.com"
 print(emailAddress.lower())
-
 # %% capitalize
 emailAddress = "luisandre_69@hotmail.com"
 print(emailAddress.capitalize())
+# %% lstring
+emailAddress = "  luisandre_69@hotmail.com  "
+print(len(emailAddress.lstrip()))
+# %% rstrip
+emailAddress = "  luisandre_69@hotmail.com  "
+print(len(emailAddress.rstrip()))
+# %% strip
+emailAddress = "  luisandre_69@hotmail.com  "
+print(len(emailAddress.rstrip()))
+# %% replace
+emailAddress = "luisandre_69@hotmail.com"
+print(emailAddress.replace("hotmail", "gmail"))
+
+# %%  find how many l exist in the string
+emailAddress = "luisandre_69@hotmail.com"
+x = 0
+for i in emailAddress:
+    if(i == 'l'):
+        x = x+1
+print(x)
+# %% fing l using replace
+emailAddress = "luisandre_69@hotmail.com"
+x = len(emailAddress)
+y = len(emailAddress.replace("l", ""))
+print(x-y)
+# %%
