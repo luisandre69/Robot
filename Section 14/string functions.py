596d5988aaac9ea2903e037f932a52f3d389eb7e
luisandre69 <luisandre_69@hotmail.com>
2020-04-21 19:53:14 +0100

    comparisons string

--- Section 14/string functions.py
+++ Section 14/string functions.py
@@ -36,4 +36,57 @@ emailAddress = "luisandre_69@hotmail.com"
 x = len(emailAddress)
 y = len(emailAddress.replace("l", ""))
 print(x-y)
+# %% Find
+emailAddress = "luisandre_69@hotmail.com"
+a = "hotmail"
+print(emailAddress.find(a))
+# returns with position it finds it.
+# %% split
+emailAddress = "This is my email luisandre_69@hotmail.com is"
+list1 = emailAddress.split(" ")
+z = 0
+for i in list1:
+    if(i == "is"):
+        z = z+1
+print(z)
+# %% copmare strings
+a = "test  "
+b = "    test"
+if(a.strip() == b.strip()):
+    print("done")
+else:
+    print("not done")
+
 # %%
+a = "TesT"
+b = "tESt"
+if(a.upper() == b.upper()):
+    print("done")
+else:
+    print("not done")
+# %%
+a = "TesT"
+b = "tESt"
+if(a.lower() == b.lower()):
+    print("done")
+else:
+    print("not done")
+# %%
+a = "testing"
+b = ""
+l = len(a)
+for i in range((l-1), -1, -1):
+    b = b+a[i]
+print(b)
+# %%
+a = "boob"
+# Reserse my string
+b = ""
+l = len(a)
+for i in range((l-1), -1, -1):
+    b = b+a[i]
+
+if(a == b):
+    print("this is a palendrome")
+else:
+    print("this isn't a palendrome")
