6347724acca32bf2e9cfafaf319357a783b4be29
luisandre69 <luisandre_69@hotmail.com>
2020-05-04 19:49:34 +0100

    tuples

--- Section 14/tuple.py
+++ Section 14/tuple.py
@@ -13,3 +13,10 @@ print(tuple1[2])
 # %%
 tuple1 = (34, 43.22, "Test", 123, "abc", 12.2)
 print(len(tuple1))
+
+# %%
+tuple1 = [34, 43.22, "Test", 123, "abc", 12.2, 34, 35, 34, 123]
+print(tuple1.count(34))
+
+# %%
+    
