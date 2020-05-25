9cf7a966ebb1ab85a531ece08038adf4079903f0
luisandre69 <luisandre_69@hotmail.com>
2020-05-25 14:05:37 +0100

    functions

--- Section 14/functions.py
+++ Section 14/functions.py
@@ -3,22 +3,53 @@ def testing():
     print("This is a test")
     print("ABC")
 
+
 testing()
 
 # %%
-def mupltiply(a,b):
+
+
+def mupltiply(a, b):
     return a*b
 
-def addition(a,b):
+
+def addition(a, b):
     return a+b
 
-# %%
-mupltiply(2,3)
 
 # %%
-addition(2,3)
+mupltiply(2, 3)
+
+# %%
+addition(2, 3)
 
 # %%
 addition(mupltiply(4, 20), 10)
 
 # %%
+
+
+def takeInput(a, b):
+    c = a+b
+    print("Sum of Values " + str(a) + " + " + str(b) + " = " + str(c))
+
+
+# %%
+takeInput(12, 3)
+
+# %%
+takeInput(b=12, a=10)
+
+# %%
+
+
+def defInput(a, b=5):
+    c = a+b
+    print("Sum of Values " + str(a) + " + " + str(b) + " = " + str(c))
+
+
+# %%
+defInput(3)
+
+# %%
+defInput(5, 3)
