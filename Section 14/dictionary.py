454b5d65701af0c9b91791a76ca6cf34a489aa46
luisandre69 <luisandre_69@hotmail.com>
2020-05-25 12:30:07 +0100

    dics

--- Section 14/dictionary.py
+++ Section 14/dictionary.py
@@ -3,3 +3,26 @@ oDics = {"K1": "Val1", 2: "val2", "K3": 42}
 print(oDics)
 
 # %%
+oDics = {"K1": "Val1", 2: "val2", "K3": 42}
+print(oDics[2])
+
+# %%
+oDics = {"K1": "Val1", 2: "val2", "K3": 42}
+oDics["K3"] = "test"
+print(oDics["K3"])
+
+# %%
+oDics = {"K1": "Val1", 2: "val2", "K3": 42}
+print(oDics.keys())
+
+# %%
+oDics = {"K1": "Val1", 2: "val2", "K3": 42}
+print(oDics.values())
+
+# %%
+oDics = {"K1": "Val1", 2: "val2", "K3": 42}
+print(oDics.items())
+
+# %%
+oDics = {"K1": "Val1", 2: "val2", "K3": 42}
+print(len(oDics))
