1ee60882264c79d0cce1c8bf2720ecbe5d9ace5a
luisandre69 <luisandre_69@hotmail.com>
2020-05-25 12:38:37 +0100

    functions

--- Section 14/functions.py
+++ Section 14/functions.py
@@ -6,3 +6,19 @@ def testing():
 testing()
 
 # %%
+def mupltiply(a,b):
+    return a*b
+
+def addition(a,b):
+    return a+b
+
+# %%
+mupltiply(2,3)
+
+# %%
+addition(2,3)
+
+# %%
+addition(mupltiply(4, 20), 10)
+
+# %%
