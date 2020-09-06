f4b42a86decc39f54f7f1a3d4f08469833101c3d
luisandre69 <luisandre_69@hotmail.com>
2020-09-06 19:12:13 +0100

    modules def

---
+++ Section 17/PyModule.py
@@ -0,0 +1,15 @@
+# This is Python Module
+# Here we are writting executable code
+
+print("Welcome Python")
+
+# Functions inside Python file(Without class), Module Functions
+
+
+def testingPythonModuleFunctions():
+    print("This is a Python Module fucntion")
+
+
+class A:
+    def testing(self):
+        print("This is a class function")
