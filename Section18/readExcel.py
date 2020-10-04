c11faaa899fb7e6e14fe56c962308fdcacb5337a
luisandre69 <luisandre_69@hotmail.com>
2020-10-04 10:36:08 +0100

    Read All Rows & Cells Data

--- Section18/readExcel.py
+++ Section18/readExcel.py
@@ -11,4 +11,29 @@ print("Active Sheet is " + vk.active.title)
 
 sh = vk['Sheet1']
 
-print(sh.title)
+print(sh['A3'].value)
+print(sh['B3'].value)
+
+c1 = sh.cell(3, 1)
+print(c1.value)
+
+print(c1.row)
+print(c1.column)
+
+# Find Row and columns Having Data
+
+rows = sh.max_row
+columns = sh.max_column
+
+print("Total Rows are - " + str(rows))
+print("Total Columns are - " + str(columns))
+
+for i in range(1, rows+1):
+    for j in range(1, columns+1):
+        c = sh.cell(i, j)
+        print (c.value)
+
+
+for r in sh['A1':'C3']:
+    for c in r:
+        print(c.value)
