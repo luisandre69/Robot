883bd6c56974edddd44355b9cf6c10ed3ecdd019
luisandre69 <luisandre_69@hotmail.com>
2020-10-28 19:16:22 +0000

    file path updated

--- Section20/ExternalKeywords/Locators.py
+++ Section20/ExternalKeywords/Locators.py
@@ -2,7 +2,7 @@ import json
 import jsonpath
 
 def read_locator_from_json(locationname):
-    f = open('C:/Users/Luis\ Abre/Document/GitHub/Robot/Section20/JsonFiles/Elements.json')
+    f = open('C:/Users/Luis Abreu/Documents/GitHub/Robot/Section20/JsonFiles/Elements.json')
     response = json.loads(f.read())
     value = jsonpath.jsonpath(response, locationname)
     return value[0]
